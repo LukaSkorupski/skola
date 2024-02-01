fa9655417df308caa3ba5ee36e9cf5493125f75b
Luka Skorupski <luka.skorupski@gmail.com>
2024-02-01 10:07:41 +0100

    Add files via upload

---
+++ 01022024_0.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bilje
+{
+    class Biljka
+    {
+
+    }
+
+    class Stablo : Biljka
+    {
+        bool otpadajuListovi;
+        public bool OtpadajuListovi { get => otpadajuListovi; set => otpadajuListovi = value; }
+        public Stablo(bool otpadajuListovi)
+        {
+            OtpadajuListovi = otpadajuListovi;
+        }
+    }
+
+    class Cvijet : Biljka
+    {
+
+    }
+
+    class Bjelogoricno : Stablo {
+        public Bjelogoricno(): base(false) { }
+    }
+    class Crnogoricno : Stablo
+    {
+        public Crnogoricno() : base(true) { }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bjelogoricno Hrast = new Bjelogoricno();
+            Crnogoricno Bor = new Crnogoricno();
+            Console.WriteLine("Hrast: {0} | Bor: {1}", Hrast.OtpadajuListovi, Bor.OtpadajuListovi);
+            Console.ReadKey();
+        }
+    }
+}
